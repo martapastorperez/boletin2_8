1273338c88f888eb1b0beb50392faccc072bccf3
mpastorperez <mpastorperez@atanasoff31.danielcastelao.org>
2016-10-05 09:34:12 +0200

    octavo ejercicio del boletin 2

---
+++ src/boletin2_8/Boletin2_8.java
@@ -0,0 +1,22 @@
+
+package boletin2_8;
+
+import javax.swing.JOptionPane;
+
+
+public class Boletin2_8 {
+
+    
+    public static void main(String[] args) {
+      float graos, kelvin,fahrenheit;
+       graos= Float.parseFloat(JOptionPane.showInputDialog("graos"));
+      
+       kelvin=graos-273;
+       fahrenheit=(graos*1.8f)/2;//f=para pasar el double a float
+       JOptionPane.showMessageDialog(null,"kelvin="+kelvin+"Fahrenheit="+fahrenheit);
+          
+        
+        
+    }
+    
+}
